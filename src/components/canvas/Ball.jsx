aaab22752610543128de2fe119d3c98bee04d4e2
nikitalokhmachev-ai <n1kkqt@gmail.com>
2024-02-23 15:12:33 -0500

    revert changes

---
+++ src/components/canvas/Ball.jsx
@@ -0,0 +1,33 @@
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { Decal, Float, OrbitControls, Preload, useTexture } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
+const Ball = (props) => {
+	const [decal] = useTexture([props.imgUrl]);
+	return (
+		<Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+			<ambientLight intensity={0.25} />
+			<directionalLight position={[0, 0, 0.05]} />
+			<mesh castShadow receiveShadow scale={2.75}>
+				<boxGeometry args={[1, 1]} />
+				<meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
+				<Decal map={decal} position={[0, 0, 1]} rotation={[0, 0, Math.PI * 2]} flatShading />
+			</mesh>
+		</Float>
+	);
+};
+
+const BallCanvas = ({ icon }) => {
+	return (
+		<Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
+			<Suspense fallback={<CanvasLoader />}>
+				<OrbitControls enableZoom={false} />
+				<Ball imgUrl={icon} />
+			</Suspense>
+			<Preload all />
+		</Canvas>
+	);
+};
+
+export default BallCanvas;
---
+++ src/components/canvas/Computers.jsx
@@ -0,0 +1,48 @@
+import { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
+const Computers = ({ isMobile }) => {
+	const computer = useGLTF("./neural_network/scene.gltf");
+	return (
+		<mesh>
+			<hemisphereLight intensity={0.15} groundColor="black" />
+			<pointLight intensity={1} />
+			<spotLight position={[-20, 50, 10]} angle={0.12} penumbra={1} intensity={1} />
+			<primitive object={computer.scene} scale={isMobile ? 0.5 : 0.7} position={isMobile ? [0, -3, -1.5] : [0, -3.25, -0.5]} rotation={[-0.01, -0.2, -0.1]} />
+		</mesh>
+	);
+};
+
+const ComputersCanvas = () => {
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		const mediaQuery = window.matchMedia("(max-width:500px)");
+
+		setIsMobile(mediaQuery.matches);
+
+		const handleMediaQueryChange = (event) => {
+			setIsMobile(event.matches);
+		};
+
+		mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+		return () => {
+			mediaQuery.removeEventListener("change", handleMediaQueryChange);
+		};
+	}, []);
+
+	return (
+		<Canvas frameloop="demand" shadows camera={{ position: [20, 3, 5], fov: 50 }} gl={{ preserveDrawingBuffer: true }}>
+			<Suspense fallback={<CanvasLoader />}>
+				<OrbitControls enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
+				<Computers />
+			</Suspense>
+			<Preload all />
+		</Canvas>
+	);
+};
+
+export default ComputersCanvas;
