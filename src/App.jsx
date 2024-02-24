99487682aeaa4faad18f2f451c9765fa7120dd16
nikitalokhmachev-ai <n1kkqt@gmail.com>
2024-02-24 03:23:00 -0500

    production ready

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
 				</div>
 				<About />
 				<Experience />
-				<Tech />
+				{/* <Tech /> */}
 				<Works />
 				<div className="relative z-0">
 					<Contact />
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,6 @@ import { useState, useRef } from "react";
 import { motion } from "framer-motion";
 import emailjs from "@emailjs/browser";
 import { styles } from "../styles";
-import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
